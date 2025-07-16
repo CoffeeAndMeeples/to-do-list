90d7a4653c8d092a92cee1ab8fd961db72b02f10
Cody Jorgensen <137858034+CoffeeAndMeeples@users.noreply.github.com>
2025-07-15 22:10:46 -0500

    Projects and Tasks populate-nonfunctioning checkboxes exist

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import "./styles.css";
 
 class Project {
-    constructor(name) {
+    constructor(name, description, due, priority) {
         this.name = name;
         this.tasks = [];
+        this.description = description;
+        this.due = due;
+        this.priority = priority;
+        this.id = makeKebab(name);
         this.selected = "False";
     }
 }
@@ -12,20 +16,40 @@ class Task {
     constructor(name) {
         this.name = name;
         this.complete = "False";
+        this.id = makeKebab(name);
     }
+}
+
+//function to convert name to kebab type to eliminate any spaces
+function makeKebab(string) {
+    const kebabed = string.replaceAll(' ', '-');
+    return kebabed;
 }
 //global variable to hold all of users projects
 const userProjects = [];
-const defaultProject = new Project("default project");
+const defaultProject = new Project("Default Project");
 userProjects.push(defaultProject);
 displayProjects(userProjects);
 updateTaskForm(defaultProject);
 
+// project form section
 const myForm = document.querySelector("#new-project");
 myForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const formData = new FormData(myForm);
-    const newProject = new Project(formData.get("name"));
+    //handle exception where project already exists
+    for (const project of userProjects) {
+        if (project.name === formData.get("name")) {
+            alert("A Project with that name already exists.");
+            return;
+        }
+    }
+    //name, description, due, priority
+    const name = formData.get("name");
+    const description = formData.get("description");
+    const due = formData.get("due");
+    const priority = formData.get("priority");
+    const newProject = new Project(name, description, due, priority);
     userProjects.push(newProject);
 
     displayProjects(userProjects);
@@ -33,6 +57,7 @@ myForm.addEventListener("submit", (event) => {
     updateTaskForm(newProject);
 })
 
+// task form section
 const taskForm = document.querySelector("#new-task");
 taskForm.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -40,15 +65,22 @@ taskForm.addEventListener("submit", (event) => {
     const newTask = new Task(formData.get("name"));
     //loop through userProjects and find the project that matches
     for (const item of userProjects) {
+        console.log(formData.get("project"));
+        console.log(item.name);
         if (item.name === formData.get("project")) {
             item.tasks.push(newTask);
-            console.log(item.tasks);
+            
             //check to see if selected project is active project
             const currentProject = document.querySelector(".selected");
-
-            //if it is, update tasks with found node
-            if (currentProject !== null && currentProject.textContent === formData.get("project")) {
-                displayTasks(currentProject);
+            console.log(currentProject);
+            if (currentProject === null) {
+                return
+            }
+            else {
+                //if it is, update tasks with found node
+                if (item.name === formData.get("project")) {
+                    displayTasks(currentProject);
+                }
             }
         }
     }
@@ -56,28 +88,40 @@ taskForm.addEventListener("submit", (event) => {
 })
 //display projects function
 function displayProjects(projectList) {
-    //grap the projects div
+    //grab the projects div
     const parentNode = document.querySelector(".projects");
     //create an array of all existing project names displayed in parentNode
     const currentlyDisplayedProjects = [];
     const projects = document.querySelectorAll(".projects > *");
     
     projects.forEach(project => {
-        currentlyDisplayedProjects.push(project.textContent);
-    })
-    console.log(currentlyDisplayedProjects);
+       const projectId = project.id;
+        currentlyDisplayedProjects.push(projectId);
+    });
     
     //put all the projects in the div
     projectList.forEach(element => {
         //compare against currentlyDisplayedProjects
-        if (!currentlyDisplayedProjects.includes(element.name)) {
+        if (!currentlyDisplayedProjects.includes(element.id)) {
             const newDiv = document.createElement("div");
-            newDiv.textContent = element.name;
+            newDiv.classList.add("project-container");
+            newDiv.classList.add(element.id);
+            newDiv.id = element.id;
+            const newLabel = document.createElement("label");
+            newLabel.for = element.id + "-checkbox";
+            newLabel.textContent = element.name;
+            const newInput = document.createElement("input");
+            newInput.type = "checkbox";
+            newInput.id = element.id + "-checkbox";
+            newInput.name = element.id + "-checkbox";
+            newDiv.appendChild(newInput);
+            newDiv.appendChild(newLabel);
             makeProjectClickable(newDiv);
             parentNode.appendChild(newDiv);
         }
     });
 }
+
 function makeProjectClickable(node) {
     node.addEventListener("click", (event) => {
         //grab the currently selected project
@@ -85,11 +129,18 @@ function makeProjectClickable(node) {
         //remove selected class
         if (currentlySelected !== null) {
         currentlySelected.classList.remove('selected');
+        //delete all the child nodes of previously selected, except the label and checkbox
+        while (currentlySelected.children.length > 2) {
+            currentlySelected.removeChild(currentlySelected.lastChild);
         }
+        }
+        //populate the node with children that hold the project's details
+        loadProjectDetails(node);
         
         //add 'selected' to the clicked node
         node.classList.add('selected');
         displayTasks(node);
+        
     })
 }
 
@@ -105,18 +156,55 @@ function updateTaskForm(project) {
 // and then create a task node for each task in selected project
 function displayTasks(node) {
     const parentNode = document.querySelector(".tasks");
-    //clear existing children
+    //clear existing children 
+    //leave the first child as it's the label for the node
     while (parentNode.children.length > 1) {
         parentNode.removeChild(parentNode.lastChild);
     }
 
     for (const project of userProjects) {
-        if (project.name === node.textContent) {
+  
+        if (project.id === node.id) {
             for (const task of project.tasks) {
                 const newNode = document.createElement("div");
-                newNode.textContent = task.name;
+                newNode.id = task.id;
+                newNode.classList.add("task-container");
+                const newLabel = document.createElement("label");
+                newLabel.for = task.id + "-checkbox";
+                newLabel.textContent = task.name;
+                const newInput = document.createElement("input");
+                newInput.type = "checkbox";
+                newInput.id = task.id + "-checkbox";
+                newInput.name = task.id + "-checkbox";
+                newNode.appendChild(newInput);
+                newNode.appendChild(newLabel);
                 parentNode.appendChild(newNode);
             }
+            project.selected = "True";
+            
+        }
+        else {
+            project.selected = "False";
         }
     }
+}
+//load a node that is the title of a project with all of it's deatails in a child node
+function loadProjectDetails(node) {
+    //create the elements
+    const container = document.createElement("div");
+    const description = document.createElement("p");
+    const due = document.createElement("p");
+    const priority = document.createElement("p");
+    //get the project from userProjects
+    for (const project of userProjects) {
+        if (node.classList.contains(project.id)) {
+            description.textContent = "Description: " + project.description;
+            due.textContent = "Due Date: " + project.due;
+            priority.textContent = "Priority: " + project.priority;
+        }
+    }
+    container.appendChild(description);
+    container.appendChild(due);
+    container.appendChild(priority);
+    node.appendChild(container);
 }
